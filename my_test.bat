1a5e4f817ffb3645352d9f3751985fd964339a6d
gojimmypi <jim@wolfssl.com>
2025-10-14 18:05:15 -0700

    remove more windows- os prefix

--- my_test.bat
+++ my_test.bat
@@ -3,6 +3,6 @@ rmdir /s /q build-stm32l4
 cmake --preset stm32l4
 
 
-:: cmake --build --preset windows-stm32l4 --parallel 4 -v
+:: cmake --build --preset stm32l4 --parallel 4 -v
 
 cmake --build --preset stm32l4
